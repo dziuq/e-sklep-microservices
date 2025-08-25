c506d7188c3189d8fb3f7bde72454345e0f1508b
PC\HomePC <dawid.dziuk@wp.pl>
2025-08-25 12:11:49 +0200

    Added db context and configurations for tables

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).HasConversion(
+                orderId => orderId.Value,
+                dbId => OrderId.Of(dbId));
+ 
+        }
+    }
+}
